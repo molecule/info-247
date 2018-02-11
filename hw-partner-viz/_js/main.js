d6fcffd68d54a6f7816f20466ab8e40d93f4a9f7
molecule <molecule@berkeley.edu>
2018-02-11 12:27:58 -0800

    update colors

--- hw-partner-viz/_js/main.js
+++ hw-partner-viz/_js/main.js
@@ -2,6 +2,38 @@
 // Highcharts API documentation
 // (available at: http://api.highcharts.com/highcharts)
 
+// function to return color
+function colors(name) {
+    var val = name.toLowerCase();
+    console.log("colors, val: " + val);
+    switch(val) {
+      case "surgicalinfection":
+        return "#A31621";
+        break;
+      case "catheterinfection":
+        return "#FA7921";
+        break;
+      case "surgicalerror":
+        return "#FCBA04";
+        break;
+      case "incorrectdosage":
+        return "#B7F0AD";
+        break;
+      case "incorrectmedicaiton":
+        return "#DDDDDD";
+        break;
+      case "delayintreatment":
+        return "#2292A4";
+        break;
+      case "misdiagnosis":
+        return "#4464AD";
+        break;
+      case "iverror":
+        return "#D1D2F9";
+        break;
+    };
+  };
+
 Highcharts.setOptions({
     lang: {
         thousandsSep: ',' // add "," at appropriate points in numbers.
@@ -87,10 +119,14 @@ $.get("_data/HospitalErrors.csv", function (data) {
                 }
             })
 
+            console.log("items[1]: " + items[0]);
+            console.log("colors: " + colors[items[0]]);
+
             if (!found) { // add a new category
                 options.series.push({
                     name: items[0],
-                    data: [Number(items[1])]
+                    data: [Number(items[1])],
+                    color: colors(items[0])
                 })
             }
 
