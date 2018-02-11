b03c7db53bb9acb14f77fe18e1dd05212fca6524
molecule <molecule@berkeley.edu>
2018-02-11 12:00:13 -0800

    removing unnecessaries

--- hw-partner-viz/_js/main.js
+++ hw-partner-viz/_js/main.js
@@ -59,26 +59,8 @@ var options = {
     }]
 };
 
-// Generate the chart (comment this out for part 2)
-//var chart = new Highcharts.Chart(options);
-
-
-//PART 2: LOADING DATA FROM A CSV USING AJAX
-
-/*Once you have the code above working and generating a chart, comment out the var 
-chart line directly above here and
-uncomment the code below to load the data from the csv using AJAX. You will need 
-to follow the instructions in the code below
-to fill in this code and pull in the data. Delete the "/*" at the beginning of the 
-next line and the "* /" at the bottom of the
-code to get started.*/
-
 // Ajax call to pull in data from the csv file data.csv
-
 $.get("_data/HospitalErrors.csv", function (data) {
-    // ***Add code here*** to create a variable "lines" that splits the csv by line using ("\n")
-    // Need a hint? The JavaScript string split method is the same as in python
-
     var lines = data.split("\n"); // Note: if using sublime2, it automatically changes line endings.
                                     // this works w/ "Windows" or "Unix" style line endings, but not "Mac"
     console.log("lines: " + lines);
@@ -99,25 +81,20 @@ $.get("_data/HospitalErrors.csv", function (data) {
                 console.log(dataCategories);
             })
         } else {
-            console.log("************** POPULATING SERIES DATA");
             var seriesData = {
                 name: "",
                 data: []
             };
             // check to see if that seriesData.name is already there, if so, add to it.
-            console.log("items[0]: " + items[0]); // Should be error type.
-            console.log(options.series);
             var found = false;
             $.each(options.series, function ( seriesNo, serie ) {
                 if (serie.name === items[0]) { // already have this category
                     serie.data.unshift(Number(items[1]));
                     found = true;
-                } else {
-
                 }
             })
 
-            if (!found) {
+            if (!found) { // add a new category
                 options.series.push({
                     name: items[0],
                     data: [Number(items[1])]
@@ -125,20 +102,6 @@ $.get("_data/HospitalErrors.csv", function (data) {
             }
 
             console.log(options.series);
-            /*
-            $.each(items, function (itemNo, item) {
-                console.log("itemNo: " + itemNo + ", item: " + item);
-                if (itemNo == 4) { // 4th element is "% mortality"
-                    console.log("item in series: " + item);
-                    seriesData.data.push(Number(item));
-                };
-            })
-            //console.log("seriesData")
-            // Now you have your var seriesData populated with the cities in name and 
-            // the temperatures in data
-            // ***Add code here*** to push seriesData to series in chart options
-            options.series.push(seriesData);
-            */
 
         };
     });
